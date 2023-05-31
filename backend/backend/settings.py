e7d023bf9add9da9842e85036f33e706e2cf057c
João Coelho <draganoid5@live.com>
2023-05-31 11:35:38 +0100

    agueda polyline delimiter adicionado

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,6 +14,17 @@ from pathlib import Path
 import os
 
 if os.name == 'nt':
+    import platform
+    OSGEO4W = r"C:\OSGeo4W"
+    if '64' in platform.architecture()[0]:
+        OSGEO4W += "64"
+    assert os.path.isdir(OSGEO4W), "Directory does not exist: " + OSGEO4W
+    os.environ['OSGEO4W_ROOT'] = OSGEO4W
+    os.environ['GDAL_DATA'] = OSGEO4W + r"\share\gdal"
+    os.environ['PROJ_LIB'] = OSGEO4W + r"\share\proj"
+    os.environ['PATH'] = OSGEO4W + r"\bin;" + os.environ['PATH'] 
+
+if os.name == 'nt':
     VENV_BASE = os.environ['VIRTUAL_ENV']
     os.environ['PATH'] = os.path.join(
         VENV_BASE, 'Lib\\site-packages\\osgeo') + ';' + os.environ['PATH']
