adb12b2789ce9c9567d20b6eee0988d77ff6242c
Tomás Paixão Rosa <tomasrosa@ua.pt>
2023-06-14 18:26:13 +0100

    fix minor bugs

--- frontend/src/Components/AgencyDetail.js
+++ frontend/src/Components/AgencyDetail.js
@@ -11,185 +11,188 @@ import defaultProfilePicture from "./Assets/defaultProfilePicture.jpg";
 
 // MUI
 import {
-	Grid,
-	AppBar,
-	Typography,
-	Button,
-	Card,
-	CardHeader,
-	CardMedia,
-	CardContent,
-	CircularProgress,
-	TextField,
-	FormControlLabel,
-	Checkbox,
-	IconButton,
-	CardActions,
+  Grid,
+  AppBar,
+  Typography,
+  Button,
+  Card,
+  CardHeader,
+  CardMedia,
+  CardContent,
+  CircularProgress,
+  TextField,
+  FormControlLabel,
+  Checkbox,
+  IconButton,
+  CardActions,
 } from "@mui/material";
 import LocalPhoneIcon from "@mui/icons-material/LocalPhone";
 
 function AgencyDetail() {
-	const navigate = useNavigate();
-	const GlobalState = useContext(StateContext);
+  const navigate = useNavigate();
+  const GlobalState = useContext(StateContext);
 
-	const params = useParams();
+  const params = useParams();
 
-	const initialState = {
-		userProfile: {
-			agencyName: "",
-			phoneNumber: "",
-			profilePic: "",
-			bio: "",
-			sellerListings: [],
-		},
-		dataIsLoading: true,
-	};
+  const initialState = {
+    userProfile: {
+      agencyName: "",
+      phoneNumber: "",
+      profilePic: "",
+      bio: "",
+      sellerListings: [],
+    },
+    dataIsLoading: true,
+  };
 
-	function ReducerFuction(draft, action) {
-		switch (action.type) {
-			case "catchUserProfileInfo":
-				draft.userProfile.agencyName = action.profileObject.agency_name;
-				draft.userProfile.phoneNumber = action.profileObject.phone_number;
-				draft.userProfile.profilePic = action.profileObject.profile_picture;
-				draft.userProfile.bio = action.profileObject.bio;
-				draft.userProfile.sellerListings = action.profileObject.seller_listings;
-				break;
+  function ReducerFuction(draft, action) {
+    switch (action.type) {
+      case "catchUserProfileInfo":
+        draft.userProfile.agencyName = action.profileObject.agency_name;
+        draft.userProfile.phoneNumber = action.profileObject.phone_number;
+        draft.userProfile.profilePic = action.profileObject.profile_picture;
+        draft.userProfile.bio = action.profileObject.bio;
+        draft.userProfile.sellerListings = action.profileObject.seller_listings;
+        break;
 
-			case "loadingDone":
-				draft.dataIsLoading = false;
-				break;
-		}
-	}
+      case "loadingDone":
+        draft.dataIsLoading = false;
+        break;
+    }
+  }
 
-	const [state, dispatch] = useImmerReducer(ReducerFuction, initialState);
+  const [state, dispatch] = useImmerReducer(ReducerFuction, initialState);
 
-	// request to get profile info
-	useEffect(() => {
-		async function GetProfileInfo() {
-			try {
-				const response = await Axios.get(
-					`http://localhost:8000/api/profiles/${params.id}/`
-				);
+  // request to get profile info
+  useEffect(() => {
+    async function GetProfileInfo() {
+      try {
+        const response = await Axios.get(
+          `http://localhost:8000/api/profiles/${params.id}/`
+        );
 
-				dispatch({
-					type: "catchUserProfileInfo",
-					profileObject: response.data,
-				});
-				dispatch({ type: "loadingDone" });
-			} catch (e) {}
-		}
-		GetProfileInfo();
-	}, []);
+        dispatch({
+          type: "catchUserProfileInfo",
+          profileObject: response.data,
+        });
+        dispatch({ type: "loadingDone" });
+      } catch (e) {}
+    }
+    GetProfileInfo();
+  }, []);
 
-	if (state.dataIsLoading === true) {
-		return (
-			<Grid
-				container
-				justifyContent="center"
-				alignItems="center"
-				style={{ height: "100vh" }}
-			>
-				<CircularProgress />
-			</Grid>
-		);
-	}
+  if (state.dataIsLoading === true) {
+    return (
+      <Grid
+        container
+        justifyContent="center"
+        alignItems="center"
+        style={{ height: "100vh" }}
+      >
+        <CircularProgress />
+      </Grid>
+    );
+  }
 
-	return (
-		<div>
-			{" "}
-			<Grid
-				container
-				style={{
-					width: "50%",
-					marginLeft: "auto",
-					marginRight: "auto",
-					border: "5px solid black",
-					marginTop: "1rem",
-					padding: "5px",
-				}}
-			>
-				<Grid item xs={6}>
-					<img
-						style={{ height: "10rem", width: "15rem" }}
-						src={
-							state.userProfile.profilePic !== null
-								? state.userProfile.profilePic
-								: defaultProfilePicture
-						}
-					/>
-				</Grid>
-				<Grid item container direction="column" justifyContent="center" xs={6}>
-					<Grid item>
-						<Typography
-							variant="h5"
-							style={{ textAlign: "center", marginTop: "1rem" }}
-						>
-							<span style={{ color: "green", fontWeight: "bolder" }}>
-								{state.userProfile.agencyName}
-							</span>
-						</Typography>
-					</Grid>
-					<Grid item>
-						<Typography
-							variant="h5"
-							style={{ textAlign: "center", marginTop: "1rem" }}
-						>
-							<IconButton>
-								<LocalPhoneIcon /> {state.userProfile.phoneNumber}
-							</IconButton>
-						</Typography>
-					</Grid>
-				</Grid>
-				<Grid item style={{ marginTop: "1rem", padding: "5px" }}>
-					{state.userProfile.bio}
-				</Grid>
-			</Grid>
-			<Grid
-				container
-				justifyContent="flex-start"
-				spacing={2}
-				style={{ padding: "10px" }}
-			>
-				{state.userProfile.sellerListings.map((listing) => {
-					return (
-						<Grid
-							key={listing.id}
-							item
-							style={{ marginTop: "1rem", maxWidth: "20rem" }}
-						>
-							<Card className="company-card">
-								<CardMedia
-									component="img"
-									height="140"
-									image={
-										listing.picture1 ? listing.picture1 : defaultProfilePicture
-									}
-									alt="Listing Picture"
-									onClick={() => navigate(`/listings/${listing.id}`)}
-									style={{ cursor: "pointer" }}
-								/>
-								<CardContent>
-									<Typography gutterBottom variant="h5" component="div">
-										{listing.title}
-									</Typography>
-									<Typography variant="body2" color="text.secondary">
-										{listing.description.substring(0, 100)}...
-									</Typography>
-								</CardContent>
-								<CardActions>
-									{`${listing.listing_type}: $${listing.vacancies
-												.toString()
-												.replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/${
-												listing.internship_bachelor
-										  }`}
-								</CardActions>
-							</Card>
-						</Grid>
-					);
-				})}
-			</Grid>
-		</div>
-	);
+  return (
+    <div>
+      {" "}
+      <Grid
+        container
+        style={{
+          width: "50%",
+          marginLeft: "auto",
+          marginRight: "auto",
+          border: "5px solid black",
+          marginTop: "1rem",
+          padding: "5px",
+        }}
+      >
+        <Grid item xs={6}>
+          <img
+            style={{ height: "10rem", width: "15rem" }}
+            src={
+              state.userProfile.profilePic !== null
+                ? state.userProfile.profilePic
+                : defaultProfilePicture
+            }
+          />
+        </Grid>
+        <Grid item container direction="column" justifyContent="center" xs={6}>
+          <Grid item>
+            <Typography
+              variant="h5"
+              style={{ textAlign: "center", marginTop: "1rem" }}
+            >
+              <span style={{ color: "green", fontWeight: "bolder" }}>
+                {state.userProfile.agencyName}
+              </span>
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography
+              variant="h5"
+              style={{ textAlign: "center", marginTop: "1rem" }}
+            >
+              <IconButton>
+                <LocalPhoneIcon /> {state.userProfile.phoneNumber}
+              </IconButton>
+            </Typography>
+          </Grid>
+        </Grid>
+        <Grid item style={{ marginTop: "1rem", padding: "5px" }}>
+          {state.userProfile.bio}
+        </Grid>
+      </Grid>
+      <Grid
+        container
+        justifyContent="flex-start"
+        spacing={2}
+        style={{ padding: "10px" }}
+      >
+        {state.userProfile.sellerListings.map((listing) => {
+          return (
+            <Grid
+              key={listing.id}
+              item
+              style={{ marginTop: "1rem", maxWidth: "20rem" }}
+            >
+              <Card className="company-card">
+                <CardMedia
+                  component="img"
+                  height="140"
+                  image={
+                    listing.picture1
+                      ? "http://localhost:8000" + listing.picture1
+                      : defaultProfilePicture
+                  }
+                  alt="Listing Picture"
+                  onClick={() => navigate(`/listings/${listing.id}`)}
+                  style={{ cursor: "pointer" }}
+                />
+
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {listing.title}
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    {listing.description.substring(0, 100)}...
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  {`${listing.listing_type}: $${listing.vacancies
+                    .toString()
+                    .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}/${
+                    listing.internship_bachelor
+                  }`}
+                </CardActions>
+              </Card>
+            </Grid>
+          );
+        })}
+      </Grid>
+    </div>
+  );
 }
 
 export default AgencyDetail;
